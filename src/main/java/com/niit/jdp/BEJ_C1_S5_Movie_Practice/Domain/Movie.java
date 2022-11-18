8d0c56f09c49bfa6de3d5ee029dabd6348648a44
M V K <krishna.mugala9@gmail.com>
2022-11-18 16:00:36 +0530

    created constructors and getters setters in movie class

--- src/main/java/com/niit/jdp/BEJ_C1_S5_Movie_Practice/Domain/Movie.java
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_Movie_Practice/Domain/Movie.java
@@ -14,4 +14,55 @@ public class Movie {
     private String directorName;
     private String actorName;
 
+    public Movie() {
+    }
+
+    public Movie(String movieId, String movieName, String directorName, String actorName) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.directorName = directorName;
+        this.actorName = actorName;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public void setDirectorName(String directorName) {
+        this.directorName = directorName;
+    }
+
+    public String getActorName() {
+        return actorName;
+    }
+
+    public void setActorName(String actorName) {
+        this.actorName = actorName;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "movieId='" + movieId + '\'' +
+                ", movieName='" + movieName + '\'' +
+                ", directorName='" + directorName + '\'' +
+                ", actorName='" + actorName + '\'' +
+                '}';
+    }
 }
