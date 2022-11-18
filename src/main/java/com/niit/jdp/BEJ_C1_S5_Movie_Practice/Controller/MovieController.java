4564a5176967546e65cf6c9e684f1f589a44002f
M V K <krishna.mugala9@gmail.com>
2022-11-18 16:47:43 +0530

    created movie controller in controller package

---
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_Movie_Practice/Controller/MovieController.java
@@ -0,0 +1,12 @@
+/*
+ * Author Name : M.Krishna.
+ * Date: 18-11-2022
+ * Created With: IntelliJ IDEA Community Edition
+ *
+ */
+
+
+package com.niit.jdp.BEJ_C1_S5_Movie_Practice.Controller;
+
+public class MovieController {
+}
--- src/main/java/com/niit/jdp/BEJ_C1_S5_Movie_Practice/Service/MovieServiceImpl.java
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_Movie_Practice/Service/MovieServiceImpl.java
@@ -8,5 +8,42 @@
 
 package com.niit.jdp.BEJ_C1_S5_Movie_Practice.Service;
 
-public class MovieServiceImpl {
+import com.niit.jdp.BEJ_C1_S5_Movie_Practice.Domain.Movie;
+import com.niit.jdp.BEJ_C1_S5_Movie_Practice.Repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+
+    private final MovieRepository movieRepository;
+
+    @Autowired
+    public MovieServiceImpl(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @Override
+    public Movie saveMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    @Override
+    public boolean deleteMovie(int movieId) {
+        movieRepository.deleteById(movieId);
+        return true;
+    }
+
+    @Override
+    public List<Movie> fetchAllMovies() {
+        return (List<Movie>) movieRepository.findAll();
+    }
+
+    @Override
+    public Optional<Movie> fetchMovieById(int movieId) {
+        return movieRepository.findById(movieId);
+    }
 }
