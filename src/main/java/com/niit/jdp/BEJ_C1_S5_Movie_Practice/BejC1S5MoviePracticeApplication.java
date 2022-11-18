9554cf990bbf7254bee2023a2d409e4d69d706bb
M V K <krishna.mugala9@gmail.com>
2022-11-18 15:57:50 +0530

    created movie class in domain class

---
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_Movie_Practice/BejC1S5MoviePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.niit.jdp.BEJ_C1_S5_Movie_Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BejC1S5MoviePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BejC1S5MoviePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niit/jdp/BEJ_C1_S5_Movie_Practice/BejC1S5MoviePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.jdp.BEJ_C1_S5_Movie_Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BejC1S5MoviePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
