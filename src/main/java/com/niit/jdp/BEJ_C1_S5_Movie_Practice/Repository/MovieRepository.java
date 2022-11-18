e32bc471cbfa158be9b09c2556f2b3451688ca13
M V K <krishna.mugala9@gmail.com>
2022-11-18 16:14:24 +0530

    added methods in the moive Service interface

--- src/main/java/com/niit/jdp/BEJ_C1_S5_Movie_Practice/Repository/MovieRepository.java
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_Movie_Practice/Repository/MovieRepository.java
@@ -1,4 +1,10 @@
 package com.niit.jdp.BEJ_C1_S5_Movie_Practice.Repository;
 
-public interface MovieRepository {
+import com.niit.jdp.BEJ_C1_S5_Movie_Practice.Domain.Movie;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends CrudRepository<Movie, Integer> {
+
 }
---
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_Movie_Practice/Service/MovieService.java
@@ -0,0 +1,16 @@
+package com.niit.jdp.BEJ_C1_S5_Movie_Practice.Service;
+
+import com.niit.jdp.BEJ_C1_S5_Movie_Practice.Domain.Movie;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MovieService {
+    Movie saveMovie(Movie movie);
+
+    boolean deleteMovie(int movieId);
+
+    List<Movie> fetchAllMovies();
+
+    Optional<Movie> fetchMovieById(int movieId);
+}
